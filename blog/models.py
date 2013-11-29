a382a15b6824cac2435c01ca9a39cf6772111574
corentinmarc <corentin.marc@gmail.com>
2013-11-29 18:26:33 +0100

    blog_home part 2

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Interview(models.Model):
-    titre = models.CharField(max_length=100)
+    artiste = models.CharField(max_length=100)
     auteur = models.CharField(max_length=42)
     contenu = models.TextField(null=False)
-    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+    date = models.DateTimeField(auto_now_add=True, auto_now=True, verbose_name="Date de parution")
     photo = models.ImageField(upload_to="blog/interview/")
     
     def __unicode__(self):
