1079bb88c5278b520be510c875f7fad30c5b5de0
corentinmarc <corentin.marc@gmail.com>
2013-12-02 02:11:30 +0100

    blog archives part1

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ from blog.models import Interview, Corpus
 def home(request):
     interview = Interview.objects.order_by('date')[0]
     return render(request, 'blog/home.html', locals())
+
+def archives(request):
+    interviews = Interview.objects.order_by('date')
+    corpuses = Corpus.objects.order_by('date')
+    return render(request, 'blog/archives.html', locals())
