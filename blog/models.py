8b93606756dbc0269ccdd064eb5acb17ef2ed197
nicolasgiannuzzi <nicolas.giann@gmail.com>
2013-12-06 15:59:44 +0100

    update

--- blog/models.py
+++ blog/models.py
@@ -2,21 +2,15 @@ from django.db import models
 
 class Interview(models.Model):
     artiste = models.CharField(max_length=100)
-    auteur = models.CharField(max_length=42)
+    auteur = models.CharField(max_length=100)
     contenu = models.TextField(null=False)
     date = models.DateTimeField(auto_now_add=True, auto_now=True, verbose_name="Date de parution")
     photo = models.ImageField(upload_to="blog/interview/")
     
     def __unicode__(self):
        return self.titre
-   
-   
-class Corpus(models.Model):
-    titre = models.CharField(max_length=100)
-    auteur = models.CharField(max_length=42)
+    
+class AnalyseCorpus(models.Model):
     contenu = models.TextField(null=False)
-    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
-    
-    
-    def __unicode__(self):
-       return self.titre
+    auteur = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="blog/analyse-corpus/")
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.template import RequestContext
-from blog.models import Interview, Corpus
+from blog.models import Interview
 
 def home(request):
     interview = Interview.objects.order_by('date')[0]
@@ -8,5 +8,4 @@ def home(request):
 
 def archives(request):
     interviews = Interview.objects.order_by('date')
-    corpuses = Corpus.objects.order_by('date')
     return render(request, 'blog/archives.html', locals())
