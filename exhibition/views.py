4fa94e02b4d500043285ec1672d29761b234e2ed
nicolasgiannuzzi <nicolas.giann@gmail.com>
2014-01-09 17:23:46 +0100

    correction + geocoder

--- exhibition/views.py
+++ exhibition/views.py
@@ -6,6 +6,8 @@ from buildengine.models import *
 from exhibition.form import *
 from work.models import *
 from buildengine.views import userBackOfficePermission
+import json
+import urllib2
 
 def manageExhibitions(request, username):
     if userBackOfficePermission(request, username):
@@ -40,18 +42,29 @@ def editExhibition(request, username, idExhibition):
         if request.method == 'POST':
             form = ExhibitionForm(request.POST)
             if form.is_valid():
-                exhibition.nameGallery = request.POST["nameGallery"]
                 exhibition.mapLongitude = request.POST["mapLongitude"]
                 exhibition.mapLatitude = request.POST["mapLatitude"]
                 exhibition.adress = request.POST["adress"]
+                print request.POST
+                if request.POST["mapLongitude"] == "" or request.POST["mapLongitude"] == "0.0":
+                    exhibition.mapLongitude = 0
+                if request.POST["mapLatitude"] == "" or request.POST["mapLatitude"] == "0.0":
+                    exhibition.mapLatitude = 0
+                URL = 'http://maps.googleapis.com/maps/api/geocode/json?address='+exhibition.adress+'&sensor=false'
+                response = urllib2.urlopen(URL)
+                data = json.load(response)
+                exhibition.mapLatitude = data["results"][0]["geometry"]["location"]["lat"]
+                exhibition.mapLongitude = data["results"][0]["geometry"]["location"]["lng"]
+                exhibition.nameGallery = request.POST["nameGallery"]
                 exhibition.zipcode = request.POST["zipcode"]
                 exhibition.country = request.POST["country"]
                 exhibition.text = request.POST["text"]
                 exhibition.date_begin = request.POST["date_begin"]
                 exhibition.date_end = request.POST["date_end"]
                 exhibition.save()
-                return render(request, 'buildengine/manageexhibition.html', {'firstname' : user.first_name, 'name' : user.last_name,
-                                                                             'exhibitions' : exhibitions})
+                #return render(request, 'buildengine/manageexhibition.html', {'firstname' : user.first_name, 'name' : user.last_name,
+                #'exhibitions' : exhibitions})
+                return HttpResponseRedirect("/"+user.username+"/build/manageexhibitions/")
         return render(request, 'form/editexhibition.html', {'firstname' : user.first_name, 'name' : user.last_name, 'form' : form, 'exhibition': exhibition})
     return HttpResponseRedirect("/")
 
