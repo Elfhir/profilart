fa326e92b0e44004e853ade4c25a24634c479109
nicolasgiannuzzi <nicolas.giann@gmail.com>
2014-01-26 19:15:58 +0100

    api tastypie

--- exhibition/api.py
+++ exhibition/api.py
@@ -1,12 +1,22 @@
 # myapp/api.py
 from tastypie.resources import ModelResource
 from exhibition.models import Exhibition
+from work.models import Work
+from django.contrib.auth.models import User
 from tastypie.serializers import Serializer
 import time
+from tastypie import fields
 from django.utils import simplejson
 from django.core.serializers import json
 
-class ExhibitionResource(ModelResource):
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.filter(is_active=True)
+        resource_name = 'user'
+        excludes = ['email', 'password', 'is_superuser', 'date_joined', 'is_staff', 'is_active', 'last_login', 'resource_uri']
+
+class ExhibitionResource(ModelResource):   
+    user = fields.ForeignKey(UserResource, 'user', full=True)
     class Meta:
         queryset = Exhibition.objects.all()
         resource_name = 'exhibition'
--- work/api.py
+++ work/api.py
@@ -1,8 +1,33 @@
 # myapp/api.py
 from tastypie.resources import ModelResource
 from work.models import Work
+from tastypie.serializers import Serializer
+from datetime import datetime
+from tastypie import fields
+from django.utils import simplejson
+from django.core.serializers import json
+from django.contrib.auth.models import User
+from tastypie.authorization import DjangoAuthorization
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'auth/user'
+        excludes = ['email', 'password', 'is_superuser', 'date_joined', 'is_staff', 'is_active', 'last_login', 'resource_uri']
 
 class WorkResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user', full=True)
     class Meta:
+        limit = 3
+        max_limit = 3
         queryset = Work.objects.all()
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get', 'post']
         resource_name = 'work'
+        authorization = DjangoAuthorization()
+        excludes = ["date_created"]
+        filtering = {
+            'user': ALL_WITH_RELATIONS,
+            'date_pub': ['exact', 'range', 'gt', 'gte', 'lt', 'lte'],
+        }
